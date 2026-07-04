d69ed7bd5d16e3223becef43dd9f515a8e76d5f3
Aman Anand <amananand9446@gmail.com>
2026-07-04 20:35:07 +0530

    Added AdderSubtractor multithreading code

---
+++ Multithreading/AdderSubtracter/Client.java
@@ -0,0 +1,15 @@
+public class Client {
+    public static void main(String[] args) {
+        System.out.println("I am the main class");
+
+        ScalerThread t1 = new ScalerThread(new Adder());
+        t1.start();
+
+        ScalerThread t2 = new ScalerThread(new Subtractor());
+        t2.start();
+
+    }
+
+
+}
+
